5d569022e430506b84045d9215ba3c08350f7c9c
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-03-04 13:48:53 -0600

    mobile version added

--- portafolio1.2/src/App.jsx
+++ portafolio1.2/src/App.jsx
@@ -8,16 +8,51 @@ import Banner2 from './Components/Banner2';
 import Skills from './Components/Skills';
 import Projects from './Components/projects';
 import Footer from './Components/Footer';
+import MobileV from './Components/Mobilev';
+
 
 
 function App() {
+
+   const getWindowDimension = () => {
+    const width = window.innerWidth
+              || document.documentElement.clientWidth
+              || document.body.clientWidth;
+    const height = window.innerHeight
+              || document.documentElement.clientHeight
+              || document.body.clientHeight;
+    console.log(width,height)
+
+    if (width < 450 ){
+      return mobileV()
+    }else{
+      return desktopV()
+    }
+    };
+
+    function mobileV(){
+      return(
+        <MobileV/>
+      )
+    }
+
+    function desktopV(){
+
+      return(
+        <div>
+          <NavBar/>
+          <Banner2/>
+          <Skills/>
+          <Projects/>
+          <Footer/>
+       </div>
+      )
+    }
+
   return (
     <div className="App">
-      <NavBar/>
-      <Banner2/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+      {getWindowDimension()}
+
 
 
     </div>
---
+++ portafolio1.2/src/Components/Mobilev.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Me from "../assets/photos/carlosNormal.jpeg";
+
+import git from '../assets/icons/github.svg';
+import instagram from '../assets/icons/instagram.svg';
+import linkdin from '../assets/icons/linkdin.svg';
+import email from '../assets/icons/email.svg'
+
+
+export default function mobileV(){
+    return(
+        <div>
+            <img src={Me} className="mobileCarlos"/>
+            <h1 className="mHeader">Carlos Martinez</h1>
+            <p className="mTxtWrap">Welcome to my personal portafolio page to see the complete version of this page please use a computer or tablet</p>
+            <h3 className="mSHeader">Contact</h3>
+
+            <div className="mIconWrap">
+                <div className="social-iconM">
+                    <a href="https://github.com/CarlosMtz1281" target="_blank"><img src={git} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/carlosmartinezrdz" target="_blank"><img src={linkdin} alt="" /></a>
+                    <a href="https://www.linkedin.com/in/carlosmartinezrdz" target="_blank"><img src={instagram} alt="" /></a>
+                    <a href="mailto:carlos.mtz@hotmail.com" target="_blank"><img src={email} alt="" /></a>
+
+                </div>
+              </div>
+
+
+        </div>
+    )
+}
