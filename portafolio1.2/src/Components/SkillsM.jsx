e4d3798497b64577ffa25a1fd840cb2deecc007d
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-03-07 15:40:18 -0600

    new skills txt

--- portafolio1.2/src/Components/SkillsM.jsx
+++ portafolio1.2/src/Components/SkillsM.jsx
@@ -7,7 +7,7 @@ export default function SkillsM(){
         <div className="skillsMobile" >
             <h1 className="MSectionTittle2">My Skills</h1>
             <img className="skillsColage" src={img}/>
-            <p id="projects">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim odit quam ratione aut natus, sapiente non fugiat temporibus, iste pariatur architecto repudiandae maxime minus alias a delectus voluptates corporis tempore!</p>
+            <p className="skillsTxtM" id="projects">During the last few years, I’ve worked on several projects that have helped me develop my coding skills. I learned Python for problem solving algorithms, C++ to build smart object-oriented systems. I’ve thought myself the basics of web design, learned HTML 5, CSS and Java Script online. I also learned React online to work as a full-time developer for Proyecto 99.</p>
         </div>
-    )
+   )
 }
