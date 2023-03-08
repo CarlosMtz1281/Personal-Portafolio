f9ce90f1979b3c3eed9782880c7366ad4d6d0f67
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-03-08 11:17:45 -0600

    txt correction

--- portafolio1.2/src/Components/AboutMeM.jsx
+++ portafolio1.2/src/Components/AboutMeM.jsx
@@ -4,7 +4,7 @@ export default function AboutMeM(){
     return(
         <div className="aboutMeM">
             <h1 className="MSectionTittle">About Me</h1>
-            <p id="skills">I'm a Software developer currently studing Computer Science at the Tecnologico de Monterey. I love to code and learn new things on my free time adn participate on competicions like Hackathons and competitive program contests</p >
+            <p id="skills">I'm a Software developer currently studying Computer Science at the Tecnologico de Monterey. I love to code,learn new things on my free time and participate on competicions like Hackathons and competitive program contests</p >
 
 
 
