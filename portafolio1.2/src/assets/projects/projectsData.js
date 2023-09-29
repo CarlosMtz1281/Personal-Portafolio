aa6f42bc6f24583f490f4c2ff84d623162c95b6e
Carlos Martinez <carlosh.mtz@hotmail.com>
2023-09-29 12:56:38 -0600

    feat(banCoach added and some syntax errors): skills, projects

--- portafolio1.2/src/assets/projects/projectsData.js
+++ portafolio1.2/src/assets/projects/projectsData.js
@@ -3,28 +3,36 @@ export default [
 
     {
         id: 1,
+        tittle: "Financial Investment Coach",
+        langTag: "React-Node.js",
+        description:" Functional web app with friendly UI that allows anyone to invest their money using AI and user information" ,
+        image:"BanCoach.png",
+
+    },
+    {
+        id: 2,
         tittle: "Inscription system for an education NPO",
-        langTag: "React",
+        langTag: "React-Node.js",
         description:" Functional inscription web page that manages inscriptions for an education non-profit organization" ,
         image:"panelP99.png",
 
     },
     {
-        id: 2,
+        id: 3,
         tittle: "UPLINK Package Delivery Prototype ",
         langTag: "React-Native",
         description:" Prototype Mobile app for an efficient package delivery system developed for a Hackathon" ,
         image:"uplink.png",
     },
     {
-        id: 3,
+        id: 4,
         tittle: "Web Page for Dental Practice",
         langTag: "HTML,CSS,JS",
         description: "Landing Page developed for a Dental Practice for promotion and instructions to the patients " ,
         image:"dentalPage.png",
     },
     {
-        id: 4,
+        id: 5,
         tittle: "Atemoc sustainable water consumption",
         langTag: "Python-Kivy",
         description:"Mobile app to monitor and optimize personal water consumption for sustainable purposes" ,
